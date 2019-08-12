6c8a8718191cae1f697b32deddc2060f8bbf1785
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-12 21:24:42 +0430

    Added : clock

--- TestBenches/Intersection_tb.v
+++ TestBenches/Intersection_tb.v
@@ -53,13 +53,11 @@ module Intersection_tb();
         .led_Vert_South_East(led_Vert_South_East), 
         .led_Vert_South_West(led_Vert_South_West)
     );
-    //always #0.5 clock =~clock;
+    always #0.5 clock =~clock;
     initial begin
         // Initialize Inputs
         $dumpfile("Intersection_tb.vcd");
         $dumpvars(0,Intersection_tb);
-
-
         clock = 0;
         police_Interrupt = 0;
         pedestrian_Hori_Street_Interrupt = 0;
@@ -69,15 +67,7 @@ module Intersection_tb();
         read_Write = 0;
         memory_Enable = 0;
         address = 0;
-        street = 0;
-
-        // Wait 100 ns for global reset to finish
-        #100;
-        clock = 1;
-        #100;
-        clock = 0;
-        #100;
-        clock = 1;  
+        street = 0;           
     end      
 endmodule
 
